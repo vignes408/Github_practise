cc9142b56eed78a730c8aad4fd57037b8ed3e1fa
VIGNESWARAN RK <Vignesrajesh02@gmail.com>
2024-03-12 09:57:50 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,5 +2,6 @@ class Sample{
   public static void main(String args[]){
     System.out.println("Good Morning");
     System.out.println("Good Night");
+    System.out.println("Good Afternoon");
   }
 }
